46c70c5a1092d0b2bd556350277c4ab190e47cb0
haritha23-dee <sumathisevagounder2308@gmail.com>
2025-07-19 12:37:32 +0530

    database configuration

--- resume_scoring/settings.py
+++ resume_scoring/settings.py
@@ -88,13 +88,14 @@ WSGI_APPLICATION = 'resume_scoring.wsgi.application'
     }
     DATABASES['default']['conn_max_age'] = 600
 else:
-    DATABASES = {
+"""
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
