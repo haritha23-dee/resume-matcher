b3cf77305e94b4e15255a6ed4f4f8188f08b8679
haritha23-dee <sumathisevagounder2308@gmail.com>
2025-07-19 13:14:54 +0530

    urls configuration

--- matcher/urls.py
+++ matcher/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.upload_resume, name='upload_resume'),
+    path('admin/', admin.site.urls),
+    path('', include('matcher.urls')),  # include your app URLs if any
 ]
+
+# To serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
